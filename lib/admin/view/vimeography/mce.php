c16cb73794f5ab2adcbb030e2fa6c4aa371567b3
Dave Kiss <hello@davekiss.com>
2014-04-08 16:21:28 -0500

    Prevent direct access

--- lib/admin/view/vimeography/mce.php
+++ lib/admin/view/vimeography/mce.php
@@ -1,8 +1,16 @@
 <?php
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 class Vimeography_MCE {
 	public function __construct() { }
 
+  /**
+   * Get the gallery id and title to show in the MCE selector
+   * 
+   * @return array
+   */
 	public function galleries() {
 		global $wpdb;
 		$galleries = $wpdb->get_results('SELECT id, title FROM '. VIMEOGRAPHY_GALLERY_TABLE);
