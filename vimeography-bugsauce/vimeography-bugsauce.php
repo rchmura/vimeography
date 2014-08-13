e506e0945cac4518ea451268e66da545b7a6e415
Dave Kiss <dave@davekiss.com>
2014-08-13 15:46:54 -0500

    Add playlist support for v1.1

--- vimeography-bugsauce/vimeography-bugsauce.php
+++ vimeography-bugsauce/vimeography-bugsauce.php
@@ -4,30 +4,27 @@ Plugin Name: Vimeography Theme: Bugsauce
 Plugin URI: http://www.vimeography.com/themes
 Theme Name: Bugsauce
 Theme URI: vimeography.com/themes/bugsauce
-Version: 1.0.2
+Version: 1.1
 Description: is the base theme that comes prepackaged with Vimeography.
 Author: Dave Kiss
 Author URI: http://www.vimeography.com/
 Copyright: Dave Kiss
 */
 
-if (! class_exists('Vimeography_Themes_Bugsauce'))
-{
-  class Vimeography_Themes_Bugsauce
-  {
+if ( ! class_exists('Vimeography_Themes_Bugsauce') ) {
+  class Vimeography_Themes_Bugsauce {
     /**
      * The current version of this theme
      *
      * @var string
      */
-    public $version = '1.0.2';
+    public $version = '1.1';
 
     /**
      * Include this theme in the Vimeography theme loader.
      */
-    public function __construct()
-    {
-      add_action('plugins_loaded', array($this, 'load_theme'));
+    public function __construct() {
+      add_action('plugins_loaded', array( $this, 'load_theme' ) );
     }
 
     /**
@@ -35,8 +32,7 @@ if (! class_exists('Vimeography_Themes_Bugsauce'))
      * @param [type] $name  [description]
      * @param [type] $value [description]
      */
-    public function __set($name, $value)
-    {
+    public function __set($name, $value) {
       $this->$name = $value;
     }
 
@@ -44,13 +40,11 @@ if (! class_exists('Vimeography_Themes_Bugsauce'))
      * Has to be public so the wp actions can reach it.
      * @return [type] [description]
      */
-    public function load_theme()
-    {
+    public function load_theme() {
       do_action('vimeography/load-theme', __FILE__);
     }
 
-    public static function load_scripts()
-    {
+    public static function load_scripts() {
       wp_dequeue_script('fitvids');
       wp_dequeue_script('flexslider');
 
@@ -67,7 +61,7 @@ if (! class_exists('Vimeography_Themes_Bugsauce'))
       wp_register_script('vimeography-pagination', VIMEOGRAPHY_ASSETS_URL.'js/pagination.js', array('jquery'));
 
       // Register our custom scripts
-      wp_register_style('bugsauce', plugins_url('media/bugsauce.css', __FILE__));
+      wp_register_style('bugsauce', plugins_url('assets/css/bugsauce.css', __FILE__));
 
       wp_enqueue_script('froogaloop');
       wp_enqueue_script('flexslider');
@@ -80,8 +74,7 @@ if (! class_exists('Vimeography_Themes_Bugsauce'))
       wp_enqueue_style('bugsauce');
     }
 
-    public function featured()
-    {
+    public function featured() {
       // optional helpers
       require_once(VIMEOGRAPHY_PATH .'lib/helpers.php');
       $helpers = new Vimeography_Helpers;
@@ -91,15 +84,15 @@ if (! class_exists('Vimeography_Themes_Bugsauce'))
       return $this->featured;
     }
 
-    public function videos()
-    {
+    public function videos() {
       // optional helpers
       require_once VIMEOGRAPHY_PATH .'lib/helpers.php';
       $helpers = new Vimeography_Helpers;
 
       // add featured video to the beginning of the array
-      if (is_array($this->featured))
-        array_unshift($this->data, $this->featured[0]);
+      if ( is_array( $this->featured ) ) {
+        array_unshift( $this->data, $this->featured[0] );
+      }
 
       return $helpers->apply_common_formatting($this->data);
     }
