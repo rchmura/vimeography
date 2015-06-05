b2d69d0533c1420ce11b96ce5b01fb678ba0082e
Dave Kiss <dave@davekiss.com>
2015-06-05 14:49:42 -0400

    Refresh the Help page design and content

--- lib/admin/controllers/vimeography/help.php
+++ lib/admin/controllers/vimeography/help.php
@@ -5,4 +5,14 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Vimeography_Help extends Vimeography_Base {
   public function __construct() { }
+
+  /**
+   * Show the Vimeography tutorials in a gallery on the Help page.
+   * Pluginception!!!!!!!
+   *
+   * @return string
+   */
+  public function tutorials_gallery() {
+    return do_shortcode("[vimeography source='https://vimeo.com/channels/vimeography' width='700px']");
+  }
 }
