94b8adac18c3a1573637b0a4730d7bcf2c29c8e3
Dave Kiss <dave@davekiss.com>
2016-07-25 16:44:36 -0400

    Limit the response fields returned in the API payload, fixes #25

--- lib/core/basic.php
+++ lib/core/basic.php
@@ -1,14 +1,36 @@
 <?php
 
-class Vimeography_Core_Basic extends Vimeography_Core
-{
-  public function __construct($settings) {
-    parent::__construct($settings);
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
 
+class Vimeography_Core_Basic extends Vimeography_Core {
+  public function __construct( $settings ) {
+    parent::__construct( $settings );
 
     $this->_auth  = VIMEOGRAPHY_ACCESS_TOKEN;
     $this->_vimeo = new Vimeography_Vimeo( NULL, NULL, $this->_auth );
     $this->_vimeo->set_user_agent( sprintf( 'Vimeography loves you (%s)', home_url() ) );
+
+    // Limit the request to return only the fields that
+    // Vimeography themes actually use.
+    $fields = apply_filters( 'vimeography.request.fields', array(
+      'name',
+      'uri',
+      'link',
+      'description',
+      'duration',
+      'width',
+      'height',
+      'embed',
+      'tags.name',
+      'created_time',
+      'stats',
+      'pictures',
+    ) );
+
+    $this->_params = array(
+      'fields' => implode( $fields, ',' )
+    );
   }
 
   /**
