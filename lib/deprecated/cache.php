d29b959223f08657711ae1c989c47a4752667cc0
Dave Kiss <dave@davekiss.com>
2017-11-08 16:15:52 -0500

    Deprecate 1.x cache class and introduce new class

--- lib/deprecated/cache.php
+++ lib/deprecated/cache.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Vimeography;
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-class Cache {
-
+class Vimeography_Cache extends Vimeography {
   /**
    * The user's cache expiration setting for the current gallery.
    *
@@ -19,59 +19,57 @@ class Cache {
    */
   private $_cache_file;
 
-
   /**
    * [__construct description]
    * @param [type] $settings [description]
    */
-  public function __construct( $engine ) {
-
+  public function __construct($gallery_id, $expiration = NULL) {
     wp_mkdir_p( VIMEOGRAPHY_CACHE_PATH );
-    $scope = is_multisite() ? get_current_blog_id() . '-' . $engine->gallery_id : $engine->gallery_id;
+    $scope = is_multisite() ? get_current_blog_id() . '-' . $gallery_id : $gallery_id;
     $this->_cache_file = VIMEOGRAPHY_CACHE_PATH . 'vimeography-gallery-' . $scope . '.cache';
 
-    if ( isset( $engine->gallery_settings['cache'] ) ) {
-      $this->_expiration = intval( $engine->gallery_settings['cache'] );
+    if ( isset($expiration) ) {
+      $this->_expiration = intval($expiration);
     }
-
   }
 
   /**
    * Checks if a cache file exists.
    *
    * @param  string $file Path to the cache file.
-   * @return bool         true if exists, false if not.
+   * @return bool         TRUE if exists, FALSE if not.
    */
   public function exists() {
-    return file_exists( $this->_cache_file );
+    return file_exists($this->_cache_file);
   }
 
-
   /**
-   * Check if the cache is expired
-   *
-   * @return bool
+   * [expired description]
+   * @param  [type] $file [description]
+   * @return [type]       [description]
    */
   public function expired() {
+    if (! isset($this->_expiration))
+      return FALSE;
 
-    if ( ! isset( $this->_expiration ) ) {
-      return false;
-    }
+    // Check if the cache is expired
+    $last_modified = @filemtime($this->_cache_file);
 
-    $last_modified = @filemtime( $this->_cache_file );
-    $expires = $last_modified + $this->_expiration;
-
-    if ( $expires < time() ) {
-      // The cache is expired, but we don't want to remove it here,
+    if (substr($this->_cache_file, -6) == '.cache' && ($last_modified + $this->_expiration) < time()) {
+      // The cache is expired, but we don't want to kill it here,
       // We only want to remove it if we do not receive a 304 response or the user forces a cache refresh.
+      // Return $last_modified to make the request along with the modified time in the header.
 
-      // Set $last_modified to make the request along with the modified time in the header.
-      $this->last_modified = date( DATE_ISO8601, $last_modified );
+      // $est = new DateTimeZone("America/New_York");
+      // $date = new DateTime();
+      // $date->setTimestamp($last_modified);
+      // $date->setTimezone($est);
+      // return $date->format(DateTime::ISO8601);
 
-      return true;
+      return date(DATE_ISO8601, $last_modified);
     }
 
-    return false;
+    return FALSE;
   }
 
   /**
@@ -83,10 +81,10 @@ class Cache {
    * @return object/bool
    */
   public function get() {
-    if ( file_exists( $this->_cache_file ) ) {
-      return unserialize( file_get_contents( $this->_cache_file ) );
+    if (file_exists($this->_cache_file)) {
+      return unserialize(file_get_contents($this->_cache_file));
     } else {
-      return false;
+      return FALSE;
     }
   }
 
@@ -99,7 +97,7 @@ class Cache {
    * @param object $video_set Vimeo collection data
    */
   public function set($data) {
-    return @file_put_contents( $this->_cache_file, serialize( $data ) );
+    return @file_put_contents($this->_cache_file, serialize($data));
   }
 
   /**
@@ -110,10 +108,10 @@ class Cache {
    * @return bool Whether the file was updated or not.
    */
   public function renew() {
-    if ( touch( $this->_cache_file ) ) {
+    if ( touch($this->_cache_file) ) {
       return $this;
     } else {
-      return false;
+      return FALSE;
     }
   }
 
@@ -124,8 +122,8 @@ class Cache {
    * @return bool          Success or failure deleting file.
    */
   public function delete() {
-    if ( substr( $this->_cache_file, -6 ) === '.cache' ) {
-      return unlink( $this->_cache_file );
+    if ( substr($this->_cache_file, -6) == '.cache' ) {
+      return unlink($this->_cache_file);
     }
   }
 
@@ -137,14 +135,13 @@ class Cache {
    * @return void
    */
   protected function cleanup() {
-    $files = scandir( VIMEOGRAPHY_CACHE_PATH );
+    $files = scandir(VIMEOGRAPHY_CACHE_PATH);
 
     foreach ($files as $file) {
-      $last_modified = @filemtime( VIMEOGRAPHY_CACHE_PATH . $file );
+      $last_modified = @filemtime(VIMEOGRAPHY_CACHE_PATH . $file);
 
-      if ( substr($file, -6) === '.cache' && ( $last_modified + $this->_expiration ) < time() ) {
-        unlink( VIMEOGRAPHY_CACHE_PATH . $file );
-      }
+      if (substr($file, -6) == '.cache' && ($last_modified + $this->_expiration) < time())
+        unlink(VIMEOGRAPHY_CACHE_PATH . $file);
     }
   }
 }
