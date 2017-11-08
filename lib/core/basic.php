b32f25c33b5dc620f7d3f2ac9c1d1a7548282d79
Dave Kiss <dave@davekiss.com>
2017-11-08 16:17:51 -0500

    Deprecate 1.x basic core and introduce new basic core

--- lib/core/basic.php
+++ lib/core/basic.php
@@ -1,23 +1,24 @@
 <?php
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+namespace Vimeography\Core;
 
-class Vimeography_Core_Basic extends Vimeography_Core {
-  public function __construct( $settings ) {
-    parent::__construct( $settings );
+class Basic extends \Vimeography\Core {
+
+  public function __construct( $engine ) {
+    parent::__construct( $engine );
 
     $this->_auth  = VIMEOGRAPHY_ACCESS_TOKEN;
     $this->_vimeo = new \Vimeo\Vimeo( null, null, $this->_auth );
   }
 
   /**
+   * Checks if the provided resource is a valid resource.
    *
-   * @param  string $resource  A Vimeo API Endpoint
+   * @param  string $resource  A Vimeo API resource
    * @return int               1 if match found, 0 if not.
    */
-  protected function _verify_vimeo_endpoint($resource) {
-    return preg_match('#^/(users|channels|albums|groups)/(.+)#', $resource);
+  protected function _verify_vimeo_resource( $resource ) {
+    return preg_match( '#^/(users|channels|albums|groups)/(.+)#', $resource );
   }
 
 }
