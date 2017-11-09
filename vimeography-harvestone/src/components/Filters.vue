886ac4ce8cf7355a6c3247020969e14b7894dee8
Dave Kiss <dave@davekiss.com>
2017-11-09 13:00:52 -0500

    Introduce new base theme

---
+++ vimeography-harvestone/src/components/Filters.vue
@@ -0,0 +1,34 @@
+<script>
+  import { mapActions, mapState } from 'vuex'
+  import Search from './Search.vue';
+  import TagContainer from './TagContainer.vue';
+
+  const template = `
+    <div class="vimeography-filters-container">  
+      <tag-container></tag-container>
+      <search v-if="this.searchEnabled"></search>
+    </div>
+  `;
+
+  const Filters = {
+    template,
+    components: {
+      TagContainer,
+      Search
+    },
+    computed: {
+      ...mapState({
+        searchEnabled: state => state.gallery.settings.search.enabled,
+      }),
+    },
+  }
+
+  export default Filters;
+</script>
+
+<style lang="scss" scoped>
+  .vimeography-filters-container {
+    display: flex;
+    justify-content: flex-end;
+  }
+</style>
---
+++ vimeography-harvestone/src/components/TagContainer.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapState } from 'vuex'
+import Tag from './Tag.vue';
+
+const template = `
+  <div class="vimeography-tag-container">
+    <tag v-for="tag in tags" :key="tag.name" :name="tag.name" :total="tag.total"></tag>
+  </div>
+`;
+
+const TagContainer = {
+  template,
+  computed: mapState({
+    tags: state => state.gallery.tags,
+  }),
+  components: {
+    Tag,
+  },
+}
+
+export default TagContainer;
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    flex: 1;
+  }
+</style>
