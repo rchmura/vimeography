9134b0d774fa448fe95621d9d3fdadb04408cef9
Dave Kiss <dave@davekiss.com>
2017-11-09 11:33:53 -0500

    Abstract store to vimeography-blueprint

--- vimeography-harvestone/src/store/mutations.js
+++
@@ -1,5 +0,0 @@
-/* Think of these as your action types */
-
-export const LOAD_VIDEO = 'LOAD_VIDEO'
-export const LOAD_VIDEO_SUCCEEDED = 'LOAD_VIDEO_SUCCEEDED'
-export const LOAD_VIDEO_FAILED = 'LOAD_VIDEO_FAILED'
