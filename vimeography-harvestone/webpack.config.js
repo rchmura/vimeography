8d42b2b67c68533ec40e9fcba5bf20f47713f92b
Dave Kiss <dave@davekiss.com>
2019-12-13 14:34:37 -0500

    Update build process and deps

--- vimeography-harvestone/webpack.config.js
+++ vimeography-harvestone/webpack.config.js
@@ -1,93 +1,102 @@
-const webpack = require('webpack');
-const path = require('path');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  devtool: isProduction ? 'cheap-module-source-map' : 'inline-source-map',
+  devtool: isProduction ? "cheap-module-source-map" : "eval-source-map",
   devServer: {
-    publicPath: 'http://localhost:8080/',
-    contentBase: './dist',
+    publicPath: "http://localhost:8080/",
+    contentBase: "./dist",
     https: true,
     hot: true,
     inline: true,
     historyApiFallback: true,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*"
     },
     watchOptions: {
       aggregateTimeout: 300,
       poll: true
     }
   },
-  target: 'web',
-  entry: './src/index',
+  target: "web",
+  entry: "./src/index",
 
   output: {
-    filename: isProduction ? 'scripts.[hash:8].js' : 'scripts.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: isProduction ? "scripts.[hash:8].js" : "scripts.js",
+    path: path.resolve(__dirname, "dist")
   },
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
-      loader: 'babel-loader'
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      exclude: /node_modules/,
-      options: {
-        postcss: [require('postcss-cssnext')()]
-      }
-    },
-    {
-      test: /\.(scss|css)$/,
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        exclude: /node_modules/,
+        options: {
+          postcss: [require("postcss-cssnext")()]
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
 
-      use: ExtractTextPlugin.extract({
-        use: [{
-          loader: 'css-loader',
-          options: {
-            sourceMap: true
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
           }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true
-          }
-        }],
-        fallback: 'style-loader'
-      })
-    }]
+        ]
+      }
+    ]
   },
 
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: "vue/dist/vue.esm.js"
     }
   },
 
   plugins: [
-    new CleanPlugin([path.resolve(__dirname, 'dist')], {
-      verbose: false
-    }),
-    new ExtractTextPlugin({
-      filename: isProduction ? 'styles.[hash:8].css' : 'styles.css',
-      disable: false
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? "styles.[hash:8].css" : "styles.css"
     }),
     new ManifestPlugin()
   ]
-}
+};
 
 if (isProduction) {
-  module.exports.plugins.push(new MinifyPlugin({
-    mangle: {
-      safari10: true
-    }
-  }));
+  module.exports.plugins.push(
+    new MinifyPlugin({
+      mangle: {
+        safari10: true
+      }
+    })
+  );
 }
