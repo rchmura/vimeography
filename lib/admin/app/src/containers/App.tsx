2f1b9da9133b601343e9c133061add2d87953ad8
Dave Kiss <dave@davekiss.com>
2021-01-06 07:07:42 -0500

    Get base react app working

---
+++ lib/admin/app/src/containers/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Component = () => {
+  return (
+    <>
+      <div className="vm-text-2xl vm-bg-red-500">Hi there! lil g</div>
+    </>
+  )
+}
+
+export default Component;
