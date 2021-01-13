0c42d30e0cd06717985cca0abc1f6e922b298eb1
Dave Kiss <dave@davekiss.com>
2021-01-13 06:05:21 -0500

    Add loading skeleton for gallery editor

--- lib/admin/app/src/components/GalleryEditor/GalleryEditor.tsx
+++ lib/admin/app/src/components/GalleryEditor/GalleryEditor.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { NavLink, Route, Switch, useRouteMatch } from "react-router-dom";
 import GalleryContext from "~/context/Gallery";
-import ThemesContext from "~/context/Themes";
 
 import AppearanceEditor from "./AppearanceEditor/AppearanceEditor";
 import BasicSettings from "./BasicSettings/BasicSettings";
@@ -93,10 +92,71 @@ const Menu = () => {
   );
 };
 
+const Skeleton = () => {
+  const [phrase, set] = React.useState(`Loading…`);
+
+  React.useEffect(() => {
+    const cb = () => {
+      const phrases = [
+        `Be right there…`,
+        `Churning the butter…`,
+        `Petting cute doggies…`,
+        `Checking the fridge again…`,
+        `Flossing…`,
+        `Loading…`,
+        `Waiting in line…`,
+        `On my way…`,
+        `Ready in a sec…`,
+        `Coming in for landing…`,
+        `Adding final touches…`,
+        `Quick restroom visit…`,
+        `Pouring coffee…`,
+        `Traffic on 77N…`,
+        `Stuck behind a slow walker…`,
+        `Waiting for rain to stop…`,
+        `Going as fast as I can…`,
+        `Almost got it…`,
+      ];
+      const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+      set(phrase);
+    };
+
+    const task = setInterval(cb, 2000);
+
+    return () => {
+      clearInterval(task);
+    };
+  }, []);
+
+  return (
+    <div
+      className="vm-bg-gray-200 vm-animate-pulse vm-w-full vm-flex vm-flex-col vm-items-center vm-justify-center vm-rounded vm-border vm-border-gray-200 vm-mt-5 vm-sticky vm-top-10 vm-text-indigo-600"
+      style={{
+        height: `30rem`,
+        backgroundImage: `linear-gradient(16deg, rgba(116, 116, 116,0.02) 0%, rgba(116, 116, 116,0.02) 25%,transparent 25%, transparent 96%,rgba(177, 177, 177,0.02) 96%, rgba(177, 177, 177,0.02) 100%),linear-gradient(236deg, rgba(148, 148, 148,0.02) 0%, rgba(148, 148, 148,0.02) 53%,transparent 53%, transparent 59%,rgba(56, 56, 56,0.02) 59%, rgba(56, 56, 56,0.02) 100%),linear-gradient(284deg, rgba(16, 16, 16,0.02) 0%, rgba(16, 16, 16,0.02) 46%,transparent 46%, transparent 71%,rgba(181, 181, 181,0.02) 71%, rgba(181, 181, 181,0.02) 100%),linear-gradient(316deg, rgba(197, 197, 197,0.02) 0%, rgba(197, 197, 197,0.02) 26%,transparent 26%, transparent 49%,rgba(58, 58, 58,0.02) 49%, rgba(58, 58, 58,0.02) 100%),linear-gradient(90deg, rgb(255,255,255),rgb(255,255,255))`,
+      }}
+    >
+      <svg
+        className="vm-w-6 vm-h-6 vm-animate-spin vm-text-indigo-800 vm-mb-2"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <div className="vm-text-xs">{phrase}</div>
+    </div>
+  );
+};
+
 const GalleryEditor = () => {
   const ctx = React.useContext(GalleryContext);
 
-  if (ctx.isLoading) return <div>Loading…</div>;
+  if (ctx.isLoading) return <Skeleton />;
 
   return (
     <div className="vm-bg-gray-100 vm-rounded vm-border vm-border-gray-200 vm-mt-5 vm-sticky vm-top-10">
