e5312cddc6d5cde47b2e50363f4f6487065adc89
Dave Kiss <dave@davekiss.com>
2021-01-13 06:03:59 -0500

    Add helpscout widget to admin editor

--- lib/admin/app/src/providers/Gallery.tsx
+++ lib/admin/app/src/providers/Gallery.tsx
@@ -423,6 +423,8 @@ const GalleryProvider = (props: GalleryProviderProps) => {
       }}
     >
       <Helmet>
+        <script type="text/javascript">{`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});`}</script>
+        <script type="text/javascript">{`window.Beacon('init', 'f7cb9d02-1ec7-4320-ad10-fcbc85e4d544')`}</script>
         <style
           type="text/css"
           id={`vimeography-gallery-${props.id}-custom-css-preview`}
