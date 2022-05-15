164e9eb1e8f92a2def217bbbd091fcf00d72f192
Dave Kiss <iamdavekiss@gmail.com>
2022-05-15 06:59:42 -0400

    Add explicit return true to schema endpoint

--- lib/api/themes.php
+++ lib/api/themes.php
@@ -28,7 +28,8 @@ class Themes extends \WP_REST_Controller
 
     register_rest_route($namespace, '/' . $base . '/schema', array(
       'methods' => \WP_REST_Server::READABLE,
-      'callback' => array($this, 'get_public_item_schema')
+      'callback' => array($this, 'get_public_item_schema'),
+      'permission_callback' => '__return_true'
     ));
   }
 
